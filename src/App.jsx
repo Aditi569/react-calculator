73465784e2592b3c4c59e3c88070be48abd04812
Aditi <your_email@example.com>
2025-01-27 16:21:42 +0530

    adding the working of the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import styles from  "./assets/App.module.css";
 import Display from "./components/Display";
 import Buttons from "./components/Buttons";
+import { useState } from "react";
+
 
 function App() {
+  const [calVal , setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+  if(buttonText === 'C'){
+    setCalVal("");
+  }else if (buttonText === '='){
+    try {
+      const result = eval(calVal);
+      setCalVal(result.toString());
+    } catch (error) {
+      setCalVal("Error");
+    }  
+   } else {
+    // Concatenate the clicked button text to the current display value
+    setCalVal((prevValue) => prevValue + buttonText);
+  }
+};
+  
   return (
     <div className={styles.calculator}>
-      <Display/>
-      <Buttons/>
+      <Display displayValue = {calVal}/>
+      <Buttons onButtonClick={onButtonClick}/>
       </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,15 +1,14 @@
 import styles from "../components/Buttons.module.css";
 
-const Buttons = () =>{
+const Buttons = ({onButtonClick}) =>{
     const buttonNames = ['C' , '1' , '2' , '3','+' , '4' , '5' ,'6' ,'-', '7' , '8' , '9' ,'*', '0' , '=' , '.' , '/']
     return(
         <>
          <div className={styles.buttonsContainer}>
          {buttonNames.map((button, index) => (
-             <button key={index} className={styles.buttons}>{button}</button>
+             <button key={index} className={styles.buttons} onClick={() => onButtonClick(button)}>{button}</button>
          ))}
         </div>
-
         </>
     )
 }
